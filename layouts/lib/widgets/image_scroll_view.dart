609763ead21421ea038d035b0adeb180778e22dd
phR0ze <prc451@gmail.com>
2023-12-04 18:08:29 -0700

    Smooth performance

--- layouts/lib/widgets/image_scroll_view.dart
+++ layouts/lib/widgets/image_scroll_view.dart
@@ -14,29 +14,38 @@ class ImageScrollView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.green,
-      child: CustomScrollView(slivers: [
-        images.isEmpty
-            ? const SliverFillRemaining(
-                child: Center(
-                  child: Text('No images found'),
-                ),
-              )
-            : SliverPadding(
-                padding: const EdgeInsets.all(2.0),
-                sliver: SliverGrid(
-                    gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                      maxCrossAxisExtent: 100.0,
-                      mainAxisSpacing: 2.0,
-                      crossAxisSpacing: 2.0,
+      child: FutureBuilder<List<String>>(
+        future: loadImages(),
+        builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
+            return CustomScrollView(slivers: [
+              snapshot.data == null || snapshot.data!.isEmpty
+                  ? const SliverFillRemaining(
+                      child: Center(
+                        child: Text('No images found'),
+                      ),
+                    )
+                  : SliverPadding(
+                      padding: const EdgeInsets.all(2.0),
+                      sliver: SliverGrid(
+                          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                            maxCrossAxisExtent: 100.0,
+                            mainAxisSpacing: 2.0,
+                            crossAxisSpacing: 2.0,
+                          ),
+                          delegate: SliverChildBuilderDelegate(
+                            (BuildContext context, int index) {
+                              return ImageTile(snapshot.data![index]);
+                            },
+                            childCount: snapshot.data!.length,
+                          )),
                     ),
-                    delegate: SliverChildBuilderDelegate(
-                      (BuildContext context, int index) {
-                        return ImageTile(images[index]);
-                      },
-                      childCount: images.length,
-                    )),
-              ),
-      ]),
+            ]);
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
     );
   }
 }
