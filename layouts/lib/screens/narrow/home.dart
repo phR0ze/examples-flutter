88bd2ab9f278482697258478813410f3c085571e
phR0ze <prc451@gmail.com>
2023-12-04 18:31:36 -0700

    If I collapse the scroll view into the home page it performs badly

--- layouts/lib/screens/narrow/home.dart
+++ layouts/lib/screens/narrow/home.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import '../../widgets/image_tile.dart';
 import '../../widgets/nav_rail.dart';
 
+// Data loader function
+Future<List<String>> loadImages() async {
+  return List.generate(2000, (index) => 'assets/images/placeholder.png');
+}
+
 class HomeScreen extends StatelessWidget {
   final BoxConstraints constraints;
   const HomeScreen(this.constraints, {super.key});
@@ -9,71 +14,67 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
-        const Expanded(child: ImageScrollView()),
-        NavRail(constraints),
-      ],
-    ));
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(color: Colors.green, child: const HomeScreenScroller()),
+          ),
+          NavRail(constraints),
+        ],
+      ),
+    );
   }
 }
 
-class ImageScrollView extends StatelessWidget {
-  const ImageScrollView({
+class HomeScreenScroller extends StatelessWidget {
+  const HomeScreenScroller({
     super.key,
   });
 
-  Future<List<String>> loadImages() async {
-    return List.generate(2000, (index) => 'assets/images/placeholder.png');
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
-      child: FutureBuilder<List<String>>(
-        future: loadImages(),
-        builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
-          if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
-            return CustomScrollView(slivers: [
-              SliverAppBar(
-                title: const Text('Narrow layout example'),
-                backgroundColor: Colors.blue,
-                expandedHeight: 200.0,
-                flexibleSpace: FlexibleSpaceBar(
-                  background: Image.asset(
-                    'assets/images/placeholder.png',
-                    fit: BoxFit.cover,
-                  ),
+    return FutureBuilder<List<String>>(
+      future: loadImages(),
+      builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
+        if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
+          return CustomScrollView(slivers: [
+            SliverAppBar(
+              title: const Text('Narrow layout example'),
+              backgroundColor: Colors.blue,
+              expandedHeight: 200.0,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Image.asset(
+                  'assets/images/placeholder.png',
+                  fit: BoxFit.cover,
                 ),
               ),
-              snapshot.data == null || snapshot.data!.isEmpty
-                  ? const SliverFillRemaining(
-                      child: Center(
-                        child: Text('No images found'),
-                      ),
-                    )
-                  : SliverPadding(
-                      padding: const EdgeInsets.all(2.0),
-                      sliver: SliverGrid(
-                          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                            maxCrossAxisExtent: 100.0,
-                            mainAxisSpacing: 2.0,
-                            crossAxisSpacing: 2.0,
-                          ),
-                          delegate: SliverChildBuilderDelegate(
-                            (BuildContext context, int index) {
-                              return ImageTile(snapshot.data![index]);
-                            },
-                            childCount: snapshot.data!.length,
-                          )),
+            ),
+            snapshot.data == null || snapshot.data!.isEmpty
+                ? const SliverFillRemaining(
+                    child: Center(
+                      child: Text('No images found'),
                     ),
-            ]);
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
+                  )
+                : SliverPadding(
+                    padding: const EdgeInsets.all(2.0),
+                    sliver: SliverGrid(
+                        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                          maxCrossAxisExtent: 100.0,
+                          mainAxisSpacing: 2.0,
+                          crossAxisSpacing: 2.0,
+                        ),
+                        delegate: SliverChildBuilderDelegate(
+                          (BuildContext context, int index) {
+                            return ImageTile(snapshot.data![index]);
+                          },
+                          childCount: snapshot.data!.length,
+                        )),
+                  ),
+          ]);
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
     );
   }
 }
