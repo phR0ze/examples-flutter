be28dda3d3c66c33ba6c9934c9b710bcc5ed7be2
phR0ze <prc451@gmail.com>
2023-12-04 18:17:15 -0700

    Split out the narrow scrolling from wide

--- layouts/lib/screens/narrow/home.dart
+++ layouts/lib/screens/narrow/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../../widgets/image_scroll_view.dart';
+import '../../widgets/image_tile.dart';
 import '../../widgets/nav_rail.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -9,15 +9,71 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Layout Examples'),
-          backgroundColor: Colors.blue,
-        ),
         body: Column(
-          children: [
-            const Expanded(child: ImageScrollView()),
-            NavRail(constraints),
-          ],
-        ));
+      children: [
+        const Expanded(child: ImageScrollView()),
+        NavRail(constraints),
+      ],
+    ));
+  }
+}
+
+class ImageScrollView extends StatelessWidget {
+  const ImageScrollView({
+    super.key,
+  });
+
+  Future<List<String>> loadImages() async {
+    return List.generate(2000, (index) => 'assets/images/placeholder.png');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+      child: FutureBuilder<List<String>>(
+        future: loadImages(),
+        builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
+            return CustomScrollView(slivers: [
+              SliverAppBar(
+                title: const Text('Narrow layout example'),
+                backgroundColor: Colors.blue,
+                expandedHeight: 200.0,
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Image.asset(
+                    'assets/images/placeholder.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              snapshot.data == null || snapshot.data!.isEmpty
+                  ? const SliverFillRemaining(
+                      child: Center(
+                        child: Text('No images found'),
+                      ),
+                    )
+                  : SliverPadding(
+                      padding: const EdgeInsets.all(2.0),
+                      sliver: SliverGrid(
+                          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                            maxCrossAxisExtent: 100.0,
+                            mainAxisSpacing: 2.0,
+                            crossAxisSpacing: 2.0,
+                          ),
+                          delegate: SliverChildBuilderDelegate(
+                            (BuildContext context, int index) {
+                              return ImageTile(snapshot.data![index]);
+                            },
+                            childCount: snapshot.data!.length,
+                          )),
+                    ),
+            ]);
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
   }
 }
