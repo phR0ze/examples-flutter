b0f57397642eac5d889228621f2d18351206df76
phR0ze <prc451@gmail.com>
2023-12-08 15:38:04 -0700

    Starting from codelab step 1

---
+++ codelab_boring_to_beautiful/android/app/src/main/kotlin/com/example/codelab_boring_to_beautiful/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.codelab_boring_to_beautiful
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- codelab_cross_platform/android/app/src/main/kotlin/com/example/codelab_cross_platform/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.codelab_cross_platform
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
