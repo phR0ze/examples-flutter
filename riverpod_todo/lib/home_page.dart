9325254c5d8d2ff6d688229ae2514452b6d87a01
phR0ze <prc451@gmail.com>
2023-12-13 17:38:39 -0700

    Working counter example

--- riverpod_todo/lib/home_page.dart
+++ riverpod_todo/lib/home_page.dart
@@ -1,31 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final state = StateProvider((ref) => 7);
+// Immutable provider state
+final titleProvider = Provider((ref) => 'Riverpod counter example');
 
-class MyHomePage extends StatelessWidget {
-  final String title;
+// Updatable provider state
+final counterProvider = StateProvider((ref) => 0);
 
-  const MyHomePage({super.key, required this.title});
+class MyHomePage extends ConsumerWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
+    final title = ref.watch(titleProvider);
+    final count = ref.watch(counterProvider);
+
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
-      ),
-      body: Center(
-        child: Consumer(builder: (context, ref, child) {
-          final count = ref.watch(state).toString();
-          return Text(count, style: Theme.of(context).textTheme.headlineLarge);
-        }),
-      ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: const Icon(Icons.add),
-      // ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        backgroundColor: Colors.lightBlue,
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              // Counter value
+              Text(
+                count.toString(),
+                style: const TextStyle(color: Colors.white, height: 5, fontSize: 23),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: ElevatedButton.icon(
+                      icon: const Icon(Icons.add),
+                      label: const Text('Add'),
+                      onPressed: () => ref.watch(counterProvider.notifier).state++,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: ElevatedButton.icon(
+                      icon: const Icon(Icons.remove),
+                      label: const Text('Minus'),
+                      onPressed: () => ref.watch(counterProvider.notifier).state--,
+                    ),
+                  ),
+                ],
+              ),
+              ElevatedButton.icon(
+                icon: const Icon(Icons.replay),
+                label: const Text('Refresh'),
+                onPressed: () => ref.watch(counterProvider.notifier).state = 0,
+              ),
+            ],
+          ),
+        ));
   }
 }
