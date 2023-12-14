c81fb1260a4e3594a14ded1101bdef0e7f0a245a
phR0ze <prc451@gmail.com>
2023-12-13 21:36:42 -0700

    Refactored counter to use Notifer

---
+++ riverpod_counter/lib/home_page.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// Immutable provider state
+final titleProvider = Provider((ref) => 'Riverpod counter example');
+
+// Couter provider state
+class Counter extends Notifier<int> {
+  @override
+  int build() {
+    return 0;
+  }
+
+  void reset() {
+    state = 0;
+  }
+
+  void dec() {
+    state--;
+  }
+
+  void inc() {
+    state++;
+  }
+}
+
+final counterProvider = NotifierProvider<Counter, int>(Counter.new);
+
+// Updatable provider state
+// final counterProvider = StateProvider((ref) => 0);
+
+class MyHomePage extends ConsumerWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final title = ref.watch(titleProvider);
+
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text(title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              // Counter value
+              Text(
+                ref.watch(counterProvider).toString(),
+                style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 48),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: ElevatedButton.icon(
+                      icon: const Icon(Icons.add),
+                      label: const Text('Add'),
+                      onPressed: () => ref.watch(counterProvider.notifier).inc(),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: ElevatedButton.icon(
+                      icon: const Icon(Icons.remove),
+                      label: const Text('Minus'),
+                      onPressed: () => ref.watch(counterProvider.notifier).dec(),
+                    ),
+                  ),
+                ],
+              ),
+              ElevatedButton.icon(
+                icon: const Icon(Icons.replay),
+                label: const Text('Refresh'),
+                onPressed: () => ref.watch(counterProvider.notifier).reset(),
+              ),
+            ],
+          ),
+        ));
+  }
+}
---
+++ riverpod_counter/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'home_page.dart';
+
+void main() {
+  runApp(const ProviderScope(
+    child: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
