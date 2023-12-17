aa6d0f2650416fce6edd9e0e311dc59199199949
phR0ze <prc451@gmail.com>
2023-12-16 18:30:36 -0700

    Convert to a Notifier class in prep for writing

--- riverpod_todo/lib/providers/todos.dart
+++ riverpod_todo/lib/providers/todos.dart
@@ -4,26 +4,30 @@ import '../model/todo.dart';
 // Generated riverpod code for todosProvider
 part 'todos.g.dart';
 
-// Defines todosProvider
+// Defines todosProvider notifier to make it writable
 @riverpod
-Future<List<Todo>> todos(TodosRef ref) async {
-  return Future.delayed(const Duration(seconds: 2), () {
-    return const [
-      Todo(
-        id: '1',
-        title: 'Example Todo 1',
-        description: 'Description 1',
-      ),
-      Todo(
-        id: '2',
-        title: 'Example Todo 2',
-        description: 'Description 2',
-      ),
-      Todo(
-        id: '3',
-        title: 'Example Todo 3',
-        description: 'Description 3',
-      ),
-    ];
-  });
+class Todos extends _$Todos {
+  // Define the initial value for the provider
+  @override
+  Future<List<Todo>> build() async {
+    return Future.delayed(const Duration(seconds: 2), () {
+      return const [
+        Todo(
+          id: '1',
+          title: 'Example Todo 1',
+          description: 'Description 1',
+        ),
+        Todo(
+          id: '2',
+          title: 'Example Todo 2',
+          description: 'Description 2',
+        ),
+        Todo(
+          id: '3',
+          title: 'Example Todo 3',
+          description: 'Description 3',
+        ),
+      ];
+    });
+  }
 }
--- riverpod_todo/lib/providers/todos.g.dart
+++ riverpod_todo/lib/providers/todos.g.dart
@@ -6,12 +6,13 @@ part of 'todos.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$todosHash() => r'36f63eec1e0513e4f1b44543e303dc170badb2bc';
+String _$todosHash() => r'd8b339a26427cff5f9d29b9928ae1c0dbab1b104';
 
-/// See also [todos].
-@ProviderFor(todos)
-final todosProvider = AutoDisposeFutureProvider<List<Todo>>.internal(
-  todos,
+/// See also [Todos].
+@ProviderFor(Todos)
+final todosProvider =
+    AutoDisposeAsyncNotifierProvider<Todos, List<Todo>>.internal(
+  Todos.new,
   name: r'todosProvider',
   debugGetCreateSourceHash:
       const bool.fromEnvironment('dart.vm.product') ? null : _$todosHash,
@@ -19,6 +20,6 @@ final todosProvider = AutoDisposeFutureProvider<List<Todo>>.internal(
   allTransitiveDependencies: null,
 );
 
-typedef TodosRef = AutoDisposeFutureProviderRef<List<Todo>>;
+typedef _$Todos = AutoDisposeAsyncNotifier<List<Todo>>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
