02473140644bbd441e689c55ae31008e79a3de5c
phR0ze <prc451@gmail.com>
2023-12-16 17:40:34 -0700

    Freezed example

--- pkg_freezed/lib/main.dart
+++ pkg_freezed/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'model/person.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -15,55 +17,57 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Freezed benefits'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
+class MyHomePage extends StatelessWidget {
   final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
+    var person = const Person(firstName: 'John', lastName: 'Doe', age: 42);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            _text('> General benefits'),
+            _text('   Ensures properties are immutable, preventing accidental mutation'),
+            _text('   Enforces null safety'),
+            _text('> Getters for all properties'),
+            _text('   firstName: ${person.firstName}'),
+            _text('   lastName: ${person.lastName}'),
+            _text('   age: ${person.age}'),
+            _text('> Operator overloads'),
+            _text('   == equality'),
+            _text('> Method helpers'),
+            _text('   copyWith(age: 43): ${person.copyWith(age: 43).age}'),
+            _text('   toString(): ${person.toString()}'),
+            _text('   hashCode: ${person.hashCode}'),
+            _text('   toJson(): ${person.toJson()}'),
+            _text('   when(): ?'),
+            _text('   maybeWhen(): ?'),
+            _text('   unions(): ?'),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
   }
+}
+
+Widget _text(String text) {
+  return Align(
+    alignment: Alignment.centerLeft,
+    child: Padding(
+      padding: const EdgeInsets.only(left: 12.0),
+      child: Text(text, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+    ),
+  );
 }
