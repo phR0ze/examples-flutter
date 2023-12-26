74eaeb2c12acbb105d539a58cf6ad7cf8041a837
phR0ze <prc451@gmail.com>
2023-12-25 23:03:46 -0700

    Include the movie page being loaded

--- riverpod_movies/lib/data/repos/tmdb_api.dart
+++ riverpod_movies/lib/data/repos/tmdb_api.dart
@@ -74,7 +74,7 @@ class TMDB {
   Future<TMDBMovies> getNowPlayingMovies({required int page}) async {
     if (page < 1 || page > 1000) throw Exception('Invalid page number');
 
-    log.red("TMDB API call invoked: getNowPlayingMovies");
+    log.red("TMDB API call invoked: getNowPlayingMovies(page: $page)");
     final response = await _query('movie/now_playing', optionalQueries: [
       'include_adult=false',
       'page=$page',
