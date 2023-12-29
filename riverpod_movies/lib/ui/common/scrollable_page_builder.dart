8eb69a58a2e86f318ed1cb109d4b886ed4f67e40
phR0ze <prc451@gmail.com>
2023-12-28 18:30:20 -0700

    Working on infinite scrolling

--- riverpod_movies/lib/ui/common/scrollable_page_builder.dart
+++ riverpod_movies/lib/ui/common/scrollable_page_builder.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../const.dart';
 import '../../providers/exports.dart';
@@ -26,6 +27,69 @@ class ScrollablePageBuilder<T extends List> extends ConsumerStatefulWidget {
 }
 
 class _PageBuilderState<T extends List> extends ConsumerState<ScrollablePageBuilder<T>> {
+  // Create a scroll controller for the GridView
+  late final ScrollController _scrollController;
+
+  // Set a threshold for when to request the next page of content
+  final _scrollThreshold = 200.0;
+
+  // Track the current page and reset back to that when the user leaves the page
+  var _currentScrollOffset = 0.0;
+
+  @override
+  void initState() {
+    // Initialize the scroll controller with listeners for attaching and detaching
+    _scrollController = ScrollController(
+      onAttach: _onAttach,
+      onDetach: _onDetach,
+    );
+
+    // Add a listener to the scroll controller to detect when the user has
+    // scrolled to the bottom of the list
+    _scrollController.addListener(_onScroll);
+
+    // Schedule jumping to the last scroll offset after the frame has been rendered
+    // SchedulerBinding.instance.addPostFrameCallback((_) {
+    //   // Only attempt to scroll if the view is attached
+    //   if (_scrollController.hasClients) {
+    //     print('TEST1');
+    //     _scrollController.jumpTo(_currentScrollOffset);
+    //   }
+    // });
+    super.initState();
+  }
+
+  void _onAttach(ScrollPosition position) {
+    print('attach');
+    //_scrollController.jumpTo(_currentScrollOffset);
+  }
+
+  void _onDetach(ScrollPosition position) {
+    print('Detach');
+  }
+
+  // Trigger a page request when the user has scrolled to the bottom of the list
+  void _onScroll() {
+    final maxOffset = _scrollController.position.maxScrollExtent;
+    print('Max: $maxOffset');
+    print('Off: ${_scrollController.offset}');
+    //if (_scrollController.offset >= maxOffset && !_scrollController.position.outOfRange) {
+    // Trigger the next page request before we reach the bottom of the list
+    if (_scrollController.offset >= maxOffset && !_scrollController.position.outOfRange) {
+      setState(() {
+        _currentScrollOffset = _scrollController.offset;
+      });
+      // widget.onNextPageRequested?.call();
+    }
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the scroll controller when the widget is disposed
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final configs = ref.watch(configProvider);
@@ -89,7 +153,9 @@ class _PageBuilderState<T extends List> extends ConsumerState<ScrollablePageBuil
                 itemCount: media.length,
                 itemBuilder: (context, index) {
                   return MediaTile(movie: media[index]);
-                }),
+                },
+                // Set the custom scroll controller to use
+                controller: _scrollController),
           );
         },
       ),
