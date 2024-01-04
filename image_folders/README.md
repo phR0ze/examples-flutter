725242c46470bc9eeb0ec63ff7a91de26cfc4895
phR0ze <prc451@gmail.com>
2024-01-04 15:06:58 -0700

    Cleaning up some of my example projects

---
+++ image_folders/README.md
@@ -0,0 +1,3 @@
+# image_folders
+Researching how to recursively read a folder and all files and folders withing that folder 
+and then display them as a thumbnail gallery. 
--- image_folders/test/widget_test.dart
+++ image_folders/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:codelab_first_app/main.dart';
+import 'package:image_folders/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
