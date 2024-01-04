f96ff316dd347a6ebc0f9ca15cdca273028e1c6e
phR0ze <prc451@gmail.com>
2024-01-04 14:51:32 -0700

    Rename for better order

--- pkg_file_picker/README.md
+++ pkg_file_picker/README.md
@@ -1,4 +1,4 @@
-# file_picker_overview
+# pkg_file_picker
 
 A new Flutter project.
 
--- pkg_file_picker/linux/my_application.cc
+++ pkg_file_picker/linux/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "file_picker_overview");
+    gtk_header_bar_set_title(header_bar, "pkg_file_picker");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "file_picker_overview");
+    gtk_window_set_title(window, "pkg_file_picker");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
