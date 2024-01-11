9bbb58196e2f4a99a818892a091c2116021c3e8d
phR0ze <prc451@gmail.com>
2024-01-10 21:30:51 -0700

    Remove old version

--- image_viewer_old/lib/utils/file.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-import 'package:path/path.dart' as path;
-
-extension FileExtention on FileSystemEntity {
-  String get name {
-    return path.basename(this.path);
-  }
-}
--- image_viewer_old/lib/widgets/extensions.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-/// Convenience methods for checking constrains for adaptive UI.
-/// e.g. `if (constraints.isDesktop)`
-extension BoxConstraintsExt on BoxConstraints {
-  bool get isTablet => maxWidth > 730;
-  bool get isDesktop => maxWidth > 1200;
-  bool get isMobile => !isTablet && !isDesktop;
-}
