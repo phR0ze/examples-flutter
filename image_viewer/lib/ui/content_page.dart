ff0c8f4b0d93a5ec59e0f7c8f202d96b09e678db
phR0ze <prc451@gmail.com>
2024-01-11 16:38:22 -0700

    Use content page for base

--- image_viewer/lib/ui/content_page.dart
+++ image_viewer/lib/ui/content_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:image_viewer/ui/text_view.dart';
+import 'text_view.dart';
 import '../model/exports.dart' as model;
+import 'common/exports.dart';
 import 'image_view.dart';
 
 class ContentPage extends StatefulWidget {
@@ -19,45 +20,34 @@ class _ContentPageState extends State<ContentPage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.entry.name, style: Theme.of(context).textTheme.titleLarge),
       ),
-      body: widget.entry.isImage
-          ? ImageView(widget.entry as model.ImageEntry)
-          : TextView(widget.entry as model.TextEntry),
-      // body: SwipeDetector(
-      //     child: Center(
-      //         child: GridTile(
-      //       header: Padding(
-      //         padding: const EdgeInsets.all(20.0),
-      //         child: Text(widget.id, style: Theme.of(context).textTheme.displayMedium),
-      //       ),
-      //       child: Image.asset(
-      //         Const.assetImagePlaceholder,
-      //         fit: BoxFit.contain,
-      //         width: MediaQuery.of(context).size.width,
-      //         height: MediaQuery.of(context).size.height,
-      //       ),
-      //     )),
-      //     onSwipeUp: () => showSnackBar('Show image details!'),
-      //     onSwipeDown: () => Navigator.pop(context),
-      //     onSwipeLeft: () => {
-      //           if (widget.index + 1 < widget.images.length - 1)
-      //             {
-      //               setState(() {
-      //                 widget.index++;
-      //                 widget.id = '1.${widget.index}';
-      //                 widget.image = widget.images[widget.index];
-      //               })
-      //             }
-      //         },
-      //     onSwipeRight: () => {
-      //           if (widget.index > 0)
-      //             {
-      //               setState(() {
-      //                 widget.index--;
-      //                 widget.id = '1.${widget.index}';
-      //                 widget.image = widget.images[widget.index];
-      //               })
-      //             }
-      //         }),
+      body: SwipeDetector(
+          child: widget.entry.isImage
+              ? ImageView(widget.entry as model.ImageEntry)
+              : TextView(widget.entry as model.TextEntry)
+
+          // onSwipeUp: () => showSnackBar('Show image details!'),
+          // onSwipeDown: () => Navigator.pop(context),
+          // onSwipeLeft: () => {
+          //   if (widget.index + 1 < widget.images.length - 1)
+          //     {
+          //       setState(() {
+          //         widget.index++;
+          //         widget.id = '1.${widget.index}';
+          //         widget.image = widget.images[widget.index];
+          //       })
+          //     }
+          // },
+          // onSwipeRight: () => {
+          //   if (widget.index > 0)
+          //     {
+          //       setState(() {
+          //         widget.index--;
+          //         widget.id = '1.${widget.index}';
+          //         widget.image = widget.images[widget.index];
+          //       })
+          //     }
+          // },
+          ),
     );
   }
 }
