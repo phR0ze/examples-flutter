55617e38b9d3a59d841d948f9101a9d7801a1327
phR0ze <prc451@gmail.com>
2024-01-11 18:05:08 -0700

    Working on swipe

--- image_viewer/lib/ui/content_page.dart
+++ image_viewer/lib/ui/content_page.dart
@@ -21,33 +21,32 @@ class _ContentPageState extends State<ContentPage> {
         title: Text(widget.entry.name, style: Theme.of(context).textTheme.titleLarge),
       ),
       body: SwipeDetector(
-          child: widget.entry.isImage
-              ? ImageView(widget.entry as model.ImageEntry)
-              : TextView(widget.entry as model.TextEntry)
-
-          // onSwipeUp: () => showSnackBar('Show image details!'),
-          // onSwipeDown: () => Navigator.pop(context),
-          // onSwipeLeft: () => {
-          //   if (widget.index + 1 < widget.images.length - 1)
-          //     {
-          //       setState(() {
-          //         widget.index++;
-          //         widget.id = '1.${widget.index}';
-          //         widget.image = widget.images[widget.index];
-          //       })
-          //     }
-          // },
-          // onSwipeRight: () => {
-          //   if (widget.index > 0)
-          //     {
-          //       setState(() {
-          //         widget.index--;
-          //         widget.id = '1.${widget.index}';
-          //         widget.image = widget.images[widget.index];
-          //       })
-          //     }
-          // },
-          ),
+        child: widget.entry.isImage
+            ? ImageView(widget.entry as model.ImageEntry)
+            : TextView(widget.entry as model.TextEntry),
+        onSwipeUp: () => showSnackBar('Show image details!'),
+        onSwipeDown: () => Navigator.pop(context),
+        // onSwipeLeft: () => {
+        //   if (widget.index + 1 < widget.images.length - 1)
+        //     {
+        //       setState(() {
+        //         widget.index++;
+        //         widget.id = '1.${widget.index}';
+        //         widget.image = widget.images[widget.index];
+        //       })
+        //     }
+        // },
+        // onSwipeRight: () => {
+        //   if (widget.index > 0)
+        //     {
+        //       setState(() {
+        //         widget.index--;
+        //         widget.id = '1.${widget.index}';
+        //         widget.image = widget.images[widget.index];
+        //       })
+        //     }
+        // },
+      ),
     );
   }
 }
