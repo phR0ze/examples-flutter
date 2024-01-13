83ac97bba6357a72fe4afa5dfe7dabe67faa1e00
phR0ze <prc451@gmail.com>
2024-01-12 20:40:02 -0700

    Worked through permissions

--- image_viewer/lib/ui/common/permissions.dart
+++ image_viewer/lib/ui/common/permissions.dart
@@ -14,7 +14,6 @@ class PermissionHandler extends StatefulWidget {
 
 class _PermissionHandlerState extends State<PermissionHandler> {
   late Future<PermissionStatus> _storagePermissionGetter;
-  bool next = false;
 
   @override
   void initState() {
@@ -24,39 +23,20 @@ class _PermissionHandlerState extends State<PermissionHandler> {
 
   // Prompt the user for permission to access the filesystem.
   //
-  // Case 1: The user has not yet been prompted
-  // * dialog is shown "Allow APP_NAME to access photos, media, and files on your device?"
-  // * user can choose "Allow" or "Deny"
+  // If the user has not yet been prompted or has already chosen 'Deny' once
+  // * a dialog is shown "Allow APP_NAME to access photos, media, and files on your device?"
+  // * the user can choose "Allow" or "Deny"
   // * choosing "Allow" completes any future requests with success without the dialog
   // * choosing "Deny" triggers the dialog again the next time a check is made
   // * choosing "Deny" a second time completes any future requests with fail without the dialog
   //
-  // Case 2:
-  //
-  // If the user has already chosen
-  // `Deny And Don't Ask Again` then open the app settings so they can change it.
   Future<PermissionStatus> _getStoragePermission() async {
     log.info('Getting storage permission');
     // Linux has no support for this
     if (Platform.isLinux) {
-      if (next) {
-        return PermissionStatus.granted;
-      }
-      return PermissionStatus.denied;
+      return PermissionStatus.granted;
     }
-
-    // Prompt the user with a dialog
-    var result = await Permission.storage.request();
-    // if (result.isGranted) {
-    //   return result;
-    // } else if (await Permission.storage.request().isPermanentlyDenied) {
-    //   await openAppSettings();
-    // } else if (await Permission.storage.request().isDenied) {
-    //   setState(() {
-    //     _storagePermissionGranted = false;
-    //   });
-    // }
-    return result;
+    return await Permission.storage.request();
   }
 
   @override
@@ -83,21 +63,17 @@ class _PermissionHandlerState extends State<PermissionHandler> {
                   ),
                   TextButton(
                     onPressed: () => setState(() {
-                      Navigator.of(context).pop();
-                      next = true;
                       _storagePermissionGetter = _getStoragePermission();
                     }),
                     child: const Text('Retry'),
                   ),
                 ],
               );
-            }
-
-            if (storagePermission.isGranted) {
-              return const Text('Permission granted');
+            } else if (storagePermission.isGranted) {
+              return widget.child ?? const SizedBox();
             } else if (storagePermission.isPermanentlyDenied) {
               return AlertDialog(
-                title: const Text('Permission denied'),
+                title: const Text('Permission denied permanently'),
                 content: const Text(
                     'Reading and writing storage is required to use this app. Please grant permission in the settings.'),
                 actions: [
@@ -108,18 +84,19 @@ class _PermissionHandlerState extends State<PermissionHandler> {
                   TextButton(
                     onPressed: () async {
                       await openAppSettings();
-                      Navigator.of(context).pop();
+                      setState(() {
+                        _storagePermissionGetter = _getStoragePermission();
+                      });
                     },
                     child: const Text('Open settings'),
                   ),
                 ],
               );
-            } else if (storagePermission.isDenied) {
-              return const Text('Permission is denied');
             }
-            return const Text('no case');
+            return const SizedBox();
           }
 
+          // Permission check failed
           if (snapshot.hasError) {
             return Center(
                 child: ErrorRetry(
