1df3c5a9d0f78c26eae97fe94c94ba9cc653fd31
phR0ze <prc451@gmail.com>
2024-01-25 16:30:35 -0700

    Adding pull_down as a package

--- pull_close/android/app/src/main/kotlin/com/example/pull_close/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.pull_close
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ pull_down/lib/pull_down.dart
@@ -0,0 +1,7 @@
+library pull_down;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
